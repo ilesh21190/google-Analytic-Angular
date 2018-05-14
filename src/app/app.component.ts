7b7dd709ff9505647305534d9d05b67e1ca7aab3
Ilesh Patel <ilesh21190@gmail.com>
2018-05-14 18:59:03 +0530

    setup angular-analytic

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component} from "@angular/core";
+import {Router, NavigationEnd} from "@angular/router";
+import {GoogleAnalyticsEventsService} from "./google-analytics-events.service";
+declare let ga: Function; 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+ 
+  constructor(public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
+  submitEvent() {
+    this.googleAnalyticsEventsService.emitEvent("testCategory", "testAction", "testLabel", 10);
+  }
+ 
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+ import {GoogleAnalyticsEventsService} from "./google-analytics-events.service";
+import { AppComponent } from './app.component';
+import {SiteAComponent} from "./site-a/site-a.component";
+import {SiteBComponent} from "./site-b/site-b.component";
+import {RouterModule} from "@angular/router";
+import {appRoutes} from "./app.routes";
+ 
+@NgModule({
+  declarations: [
+    AppComponent,
+    SiteAComponent,
+    SiteBComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [GoogleAnalyticsEventsService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
